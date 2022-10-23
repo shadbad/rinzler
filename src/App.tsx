47008295d0944962a5f8b37b4e04326fa9573d09
Sina Shadbad <shadbad@gmail.com>
2022-10-23 17:28:09 +0100

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ErrorBoundary, Landing } from 'components/pages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <ErrorBoundary>
+            <Router basename="/">
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                </Routes>
+            </Router>
+        </ErrorBoundary>
+    );
 }
-
-export default App;
